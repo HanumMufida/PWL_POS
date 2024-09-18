3d6cf7fb9a62054a8d088e76d90acd41d60736e8
= Hanum Mufida Akhsanti <= hanumakhsanti@gmail.com>
2024-09-18 08:11:55 +0700

    Js04_P01

---
+++ app/Models/UserModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primarykey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    
+    protected $fillable =['level_id', 'username','nama'];
+}
