1302a366cf05206e2ff8b0819db3d4e7c5758a6d
= Hanum Mufida Akhsanti <= hanumakhsanti@gmail.com>
2024-09-18 09:49:50 +0700

    Js04_P2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function index()
@@ -27,14 +28,26 @@ class UserController extends Controller
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data'=> $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'manager 3',
-            'password'=> Hash::make('12345')
-        ];
-        UserModel::create($data);
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'manager 3',
+        //     'password'=> Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user',['data'=> $user]);
+
+        // $user = UserModel::firstWhere('level_id',1);
+        $user = UserModel::findOr(20,['username','nama' ], function(){
+            abort(404);
+        });
+        return view('user',['data'=> $user]);
+
+        
+        
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,12 +9,13 @@ class UserModel extends Model
 {
     use HasFactory;
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primarykey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    
     /**
      * The attributes that are mass assignable
      * 
      * @var array
      */
     
-    protected $fillable =['level_id', 'username','nama'];
+    // protected $fillable =['level_id', 'username','nama','password'];
 }
