01411da02c364948c95a85120fd267e99b2f08b3
= Hanum Mufida Akhsanti <= hanumakhsanti@gmail.com>
2024-10-09 11:05:56 +0700

    Js07_P3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,11 +13,13 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
-        $user = $request->user();   //ambil data user yang login
-        if($user->hasRole($role)){
-            return $next($request); 
+        $user_role = $request->user()->getRole();
+        
+        
+        if (in_array($user_role, $roles)) {
+            return $next($request);
         }
         
         abort(403, 'Forbidden, kamu tidak punya akses ke halaman ini');
