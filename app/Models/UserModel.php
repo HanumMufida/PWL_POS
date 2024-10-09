bd535d6c500a0c2d6df362eaf23b54399a63ea04
= Hanum Mufida Akhsanti <= hanumakhsanti@gmail.com>
2024-10-09 08:37:25 +0700

    Js07_P1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi class Authenticatable
 
 class UserModel extends Model
 {
@@ -12,7 +13,13 @@ class UserModel extends Model
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
     
-    protected $fillable =['level_id', 'username','nama','password'];
+    protected $fillable =['username','password','nama','level_id','created_at','updated_at' ];
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+    protected $casts =['password' => 'hashed']; //casting password agar otomatis hash
+
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class,'level_id','level_id');
